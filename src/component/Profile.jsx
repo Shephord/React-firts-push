bdded3fbbcee16cbe806129495f6746d662f63cc
Shephord <andriibunesku@gmail.com>
2023-05-14 20:57:44 +0200

    added same stile

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -9,15 +9,30 @@ function Profile() {
           src='https://wallpapers.com/images/hd/1200x480-trippy-cloudy-galaxy-o884x4wppksi3lkw.jpg'
         />
       </div>
-
-      <div>
-        <img
-          className={classes.profile}
-          alt='#'
-          src='https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-        />
+      <div className={classes.profile}>
+        <div>
+          <img
+            className={classes.profile__img}
+            alt='#'
+            src='https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+          />
+        </div>
+        <div className={classes.profile_content}>
+          <h2>Flayj Tril</h2>
+          <div>
+            Birth Day: <spam>09/12/1994</spam>
+          </div>
+          <div>
+           City: <spam>Venice</spam>
+          </div>
+          <div>
+            Education: <spam>BSU'11</spam>
+          </div>
+          <div>
+            WEB Site: <a href="https://google.com">Google</a>
+          </div>
+        </div>
       </div>
-
       <div>
         My post
         <div className={`${classes.post} ${classes.active} `}>new Post</div>
--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,33 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUser, faMessage,faNewspaper,faMusic,faGear } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faMessage,
+  faNewspaper,
+  faMusic,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
 import classes from "./nav.module.css";
 function Nav() {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
         {" "}
-        <a alt='#f' href='#f'>
-        <FontAwesomeIcon icon={faUser} />  Profile
+        <a href='#f'>
+          <FontAwesomeIcon icon={faUser} /> Profile
         </a>
       </div>
       <div className={classes.item}>
-        <a alt='#f' href='#f'>
-        <FontAwesomeIcon icon={faMessage} />  Messages
+        <a href='#f'>
+          <FontAwesomeIcon icon={faMessage} /> Messages
         </a>
       </div>
       <div className={classes.item}>
-        <a alt='#f' href='#f'>
-        <FontAwesomeIcon icon={faNewspaper} /> News
+        <a href='#f'>
+          <FontAwesomeIcon icon={faNewspaper} /> News
         </a>
       </div>
       <div className={classes.item}>
-        <a alt='#f' href='#f'>
-        <FontAwesomeIcon icon={faMusic} />  Music
+        <a href='#f'>
+          <FontAwesomeIcon icon={faMusic} /> Music
         </a>
       </div>
       <div className={classes.item}>
-        <a alt='#f' href='#f'>
-        <FontAwesomeIcon icon={faGear} /> Settings
+        <a href='#f'>
+          <FontAwesomeIcon icon={faGear} /> Settings
         </a>
       </div>
     </nav>
