8de70f791fc2d3e266c2d12173659ca695521beb
Shephord <andriibunesku@gmail.com>
2023-07-23 15:33:09 +0200

    code refactoring

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -2,22 +2,26 @@ import classes from "./Dialogs.module.css";
 import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message";
 
-
 function Dialogs(props) {
   return (
     <div className={`${classes.dialogs}  ${"content"}`}>
       {" "}
       <div className={classes.dialogs_item}>
-        {props.dialogs.map((dialog) => (
-          <Dialog name={dialog.name} id={dialog.id} url={dialog.url} />
-        ))}
+        <div className={classes.dialogs_person}>
+          {props.dialogs.map((dialog) => (
+            <Dialog name={dialog.name} id={dialog.id} url={dialog.url} />
+          ))}
+        </div>
+        <div className={classes.dialogs_messages }>
+          {props.messages.map((messageData) => (
+            <Message message={messageData.message} url={messageData.url} />
+          ))}
+        </div>
       </div>
-      <div className={classes.dialogs_messages}>
-        {props.messages.map((messageData) => (
-          <Message message={messageData.message} url={messageData.url} />
-        ))}
+      <div className={classes.dialogs_input}>
+        <input type='text' />
+        <button>Send</button>
       </div>
-      <div><input type="text" /></div>
     </div>
   );
 }
